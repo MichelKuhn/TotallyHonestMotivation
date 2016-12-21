2b525bc47cf05cec8be67da01824873ed1262a46
michel <michel.kuhn@live.com>
2016-12-21 14:24:45 +0100

    Added simple quoteGenerator structure

---
+++ app/src/main/java/com/example/michel/totallyhonestmotivation/QuoteGenerator.java
@@ -0,0 +1,11 @@
+package com.example.michel.totallyhonestmotivation;
+
+/**
+ * Created by michel on 21.12.16.
+ */
+
+public class QuoteGenerator {
+    public String generateQuote() {
+        return "Wo ein Wille ist, ist auch ein Steak.";
+    }
+}
