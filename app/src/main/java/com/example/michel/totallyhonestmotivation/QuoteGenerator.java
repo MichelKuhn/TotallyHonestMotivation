a2bc723dce286f21e2fd9d80c4403fc561b4fc4a
michel <michel.kuhn@live.com>
2016-12-22 12:47:23 +0100

    Implemented simple randomness for the words in the QUoteGenerator

--- app/src/main/java/com/example/michel/totallyhonestmotivation/QuoteGenerator.java
+++ app/src/main/java/com/example/michel/totallyhonestmotivation/QuoteGenerator.java
@@ -1,4 +1,6 @@
 package com.example.michel.totallyhonestmotivation;
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by michel on 21.12.16.
@@ -8,11 +10,29 @@ public class QuoteGenerator {
     private static String ADJECTIVE_PATTERN = "\\[Adjektiv\\]";
     private static String SUBSTANTIV_PATTERN = "\\[Substantiv\\]";
 
-    private static String template = "Du bist [Adjektiv], denn du bist ein [Substantiv].";
+    private static String template = "[Substantiv] ist [Adjektiv], denn du bist ein Mensch.";
+
+    private static ArrayList<String> adjektive = new ArrayList<String>();
+    private static ArrayList<String> substantive = new ArrayList<String>();
+
+    public QuoteGenerator() {
+        adjektive.add("wundervoll");
+        adjektive.add("traumhaft");
+        substantive.add("Leben");
+        substantive.add("Nebel");
+    }
+
+    private static String createRandomWord(String type) {
+        switch (type) {
+            case "ADJEKTIV": return adjektive.get(new Random().nextInt(adjektive.size()));
+            case "SUBSTANTIV": return substantive.get(new Random().nextInt(substantive.size()));
+            default: return null;
+        }
+    }
 
     private static String replaceWords(String quote) {
-        String temp = quote.replaceAll(ADJECTIVE_PATTERN, "schick");
-        return temp.replaceAll(SUBSTANTIV_PATTERN, "Huhn");
+        String temp = quote.replaceAll(ADJECTIVE_PATTERN, createRandomWord("ADJEKTIV"));
+        return temp.replaceAll(SUBSTANTIV_PATTERN, createRandomWord("SUBSTANTIV"));
     }
 
     public String generateQuote() {
